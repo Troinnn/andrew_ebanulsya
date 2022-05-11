480d7b1ece84276797f00d2f0df2b42f9b4b8b8b
asemenov <asemenov@airba.kz>
2022-05-11 13:18:13 +0600

    список и одиночный объект без пагинации

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/render v1.0.1
 	go.mongodb.org/mongo-driver v1.9.1
 )
 
--- link_resource.go
+++ link_resource.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 )
@@ -26,9 +29,51 @@ func (lr LinkResource) newRedirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func (lr LinkResource) redirect(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id := chi.URLParam(r, "id")
 
+	find := lr.col.FindOne(ctx, bson.D{
+		{
+			Key:   "_id",
+			Value: id,
+		},
+	}, nil)
+
+	var link Link
+	err := find.Decode(&link)
+	if err != nil {
+		render.Status(r, http.StatusInternalServerError)
+		render.NoContent(w, r)
+		return
+	}
+
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, link)
 }
 
 func (lr LinkResource) redirects(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
+	find, err := lr.col.Find(ctx, bson.D{})
+	if err != nil {
+		render.Status(r, http.StatusInternalServerError)
+		render.NoContent(w, r)
+		return
+	}
+
+	links := make([]Link, 0, 1)
+
+	for find.Next(ctx) {
+		var link Link
+		err := find.Decode(&link)
+		if err != nil {
+			render.Status(r, http.StatusInternalServerError)
+			render.NoContent(w, r)
+			return
+		}
+		links = append(links, link)
+	}
+
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, links)
 }
